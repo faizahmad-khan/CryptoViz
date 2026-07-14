179b81d63c73c40eebd05e7fa6b7765346cebf0c
Ruchira807 <nayaksuman2014@gmail.com>
2026-07-14 18:20:23 +0530

    fix: use resource id as React key

--- components/resources/SearchBar.tsx
+++ components/resources/SearchBar.tsx
@@ -23,7 +23,7 @@ export default function SearchBar({ resources }: Props) {
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {resources.map((resource) => (
         <ResourceCard
-          key={resource.title}
+          key={resource.id}
           resource={resource}
         />
       ))}
